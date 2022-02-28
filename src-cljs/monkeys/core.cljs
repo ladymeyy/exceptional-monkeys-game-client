9fe97441689ac8c98d99e7cd5c5392da7809b2c5
James Tolton <jjtolton@gmail.com>
2022-02-28 16:42:34 -0500

    revert to working adaptation of React code

--- src-cljs/monkeys/core.cljs
+++ src-cljs/monkeys/core.cljs
@@ -2,21 +2,22 @@
   (:require reagent.dom
             [reagent.core :as r]))
 
-(def use-websocket js/useWebSocket)
-(def ws js/ws)
-(def use-body-bounderies js/useBodyBounderies)
-(def player-moves js/playerMoves)
-(def use-event-listener js/useEventListener)
-(def monkeys (r/adapt-react-class js/Monkeys))
-(def score (r/adapt-react-class js/Score))
-(def dialog (r/adapt-react-class js/Dialog))
-(def exceptions (r/adapt-react-class js/Exceptions))
+(comment
+  (def use-websocket js/useWebSocket)
+  (def ws js/ws)
+  (def use-body-bounderies js/useBodyBounderies)
+  (def player-moves js/playerMoves)
+  (def use-event-listener js/useEventListener)
+  (def monkeys (r/adapt-react-class js/Monkeys))
+  (def score (r/adapt-react-class js/Score))
+  (def dialog (r/adapt-react-class js/Dialog))
+  (def exceptions (r/adapt-react-class js/Exceptions))
 
-(defn app []
-  (let [body-bounderies (use-body-bounderies)
-        player-moves (player-moves)]))
-
+  ;; uncomment when rewriting app component
+  (defn app []
+    (let [body-bounderies (use-body-bounderies)
+          player-moves (player-moves)])))
 
 (defn ^:export init []
-  (reagent.dom/render [app] (.-body js/document)))
+  (reagent.dom/render [(r/adapt-react-class js/App)] (.-body js/document)))
 
--- src/index.js
+++ src/index.js
@@ -7,21 +7,23 @@ import App from './App';
 window.App = App;
 
 
-import { useWebSocket } from './hooks/useWebSocket';
-import { useBodyBounderies } from './hooks/useBodyBounderies'
-import { ws, playerMoves } from './Utils.js/Utils'
-import { useEventListener } from './hooks/useEventListener'
-import Monkeys from './components/Monkeys'
-import Score from './components/score/Score'
-import Dialog from './components/dialog/Dialog'
-import Exceptions from './components/exceptions/Exceptions'
+// uncomment when rewriting <App/>
 
-window.useWebSocket = useWebSocket;
-window.useBodyBounderies = useBodyBounderies;
-window.ws = ws;
-window.playerMoves = playerMoves;
-window.useEventListener = useEventListener;
-window.Monkeys = Monkeys;
-window.Score = Score;
-window.Dialog = Dialog;
-window.Exceptions = Exceptions;
+// import { useWebSocket } from './hooks/useWebSocket';
+// import { useBodyBounderies } from './hooks/useBodyBounderies'
+// import { ws, playerMoves } from './Utils.js/Utils'
+// import { useEventListener } from './hooks/useEventListener'
+// import Monkeys from './components/Monkeys'
+// import Score from './components/score/Score'
+// import Dialog from './components/dialog/Dialog'
+// import Exceptions from './components/exceptions/Exceptions'
+
+// window.useWebSocket = useWebSocket;
+// window.useBodyBounderies = useBodyBounderies;
+// window.ws = ws;
+// window.playerMoves = playerMoves;
+// window.useEventListener = useEventListener;
+// window.Monkeys = Monkeys;
+// window.Score = Score;
+// window.Dialog = Dialog;
+// window.Exceptions = Exceptions;
